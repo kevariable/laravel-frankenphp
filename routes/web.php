d773968d27d0f80e80117892d1e44a70f039a1c2
Kevin Abrar Khansa <kevariable@gmail.com>
2024-06-11 21:55:02 +0700

    Update Dockerfile installation

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $fiber = new Fiber(function() {
-        echo 'In the Fiber'.PHP_EOL;
-        echo 'Still inside'.PHP_EOL;
-    });
-    $fiber->start();
+    return \App\Models\User::all();
 });
